480b27c49988e35bd0ea45db1378daf416eccab0
Jackson Hong <jacksonxxhong@gmail.com>
2016-10-12 14:03:02 -0400

    Error handling for invalid timezones

--- server.go
+++ server.go
@@ -13,21 +13,26 @@ func UnixTimestamp(w http.ResponseWriter, r *http.Request, _ httprouter.Params) 
     fmt.Fprintf(w, "%d", now)
 }
 
-//func TimeIn(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	//now := time.Now()
-	//locationString := ps.ByName("location")
-	//location, err := time.LoadLocation(locationString)
+func TimeIn(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	now := time.Now()
+	tz := ps.ByName("tz")
+	timezone, err := time.LoadLocation(tz)
 
-	//fmt.Fprintf(w, "%d", now.In(location))
-//}
+	if err != nil {
+		fmt.Fprintf(w, "Invalid timezone.")
+		return
+	}
+
+	fmt.Fprintf(w, "%s", now.In(timezone).String())
+}
 
 func main() {
 	router := httprouter.New()
 	// Routes
 	router.GET("/", UnixTimestamp)
-	//router.GET("/:location", TimeIn)
+	router.GET("/:tz", TimeIn)
 
 	// Start server
-	fmt.Printf("Listening on port 8080...")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	fmt.Printf("Listening on port 3001...")
+	log.Fatal(http.ListenAndServe(":3001", router))
 }
